54f08c8622178221dcc7388ab75cb2be8136504f
Tiago Costa <tiagodpv@gmail.com>
2023-03-04 23:31:27 -0500

    add fireb

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
+import Todo from './Todo';
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5ED]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 rounded-md text-slate-100`,
+  count: `text-center p-2`,
 };
 
 function App() {
+  const [todos, setTodos] = useState(['Learn React', 'Grind Leetcode']);
+
   return (
     <div className={style.bg}>
       <div className={style.container}>
@@ -16,6 +25,12 @@ function App() {
             <AiOutlinePlus size={30} />
           </button>
         </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+        <p className={style.count}>You have 2 todos</p>
       </div>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAl34XCFkBqvAkp0nCKYpMzzTXRLC3DaJQ',
+  authDomain: 'todo-app-yt-741e9.firebaseapp.com',
+  projectId: 'todo-app-yt-741e9',
+  storageBucket: 'todo-app-yt-741e9.appspot.com',
+  messagingSenderId: '986886647106',
+  appId: '1:986886647106:web:f7c8c4a488de7a6da32cdb',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
